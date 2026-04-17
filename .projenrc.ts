08a19df0825b606b66a492c473aa1dbf83ecfdd0
yicr <yicr@users.noreply.github.com>
2026-04-17 08:30:58 +0000

    chore(deps): add deps packages

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,4 @@
-import { awscdk, javascript } from 'projen';
+import { awscdk, javascript, github } from 'projen';
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'yicr',
   authorAddress: 'yicr@users.noreply.github.com',
@@ -7,8 +7,67 @@ const project = new awscdk.AwsCdkConstructLibrary({
   typescriptVersion: '5.9.x',
   jsiiVersion: '5.9.x',
   name: 'daily-cloudwatch-logs-archiver',
-  packageManager: javascript.NodePackageManager.NPM,
+  packageManager: javascript.NodePackageManager.YARN_CLASSIC,
   projenrcTs: true,
   repositoryUrl: 'https://github.com/gammarers-aws-cdk-constructs/daily-cloudwatch-logs-archiver.git',
+  deps: [],
+  devDeps: [
+    '@aws/durable-execution-sdk-js@^1',
+    '@aws-sdk/client-cost-explorer@^3',
+    '@aws-sdk/client-lambda@^3',
+    '@aws-sdk/client-rds@^3',
+    '@aws-sdk/client-resource-groups-tagging-api@^3',
+    '@slack/web-api@^6',
+    '@types/aws-lambda@^8',
+    'aws-lambda-secret-fetcher@^0.3',
+    'aws-sdk-client-mock@^2',
+    'aws-sdk-client-mock-jest@^2',
+    'safe-env-getter@^0.1',
+  ],
+  releaseToNpm: true,
+  // npmTrustedPublishing: true,
+  npmAccess: javascript.NpmAccess.PUBLIC,
+  mergify: true,
+  minNodeVersion: '20.0.0',
+  workflowNodeVersion: '24.x',
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      schedule: javascript.UpgradeDependenciesSchedule.WEEKLY,
+    },
+  },
+  githubOptions: {
+    projenCredentials: github.GithubCredentials.fromApp({
+      permissions: {
+        pullRequests: github.workflows.AppPermission.WRITE,
+        contents: github.workflows.AppPermission.WRITE,
+        workflows: github.workflows.AppPermission.WRITE,
+      },
+    }),
+  },
+  autoApproveOptions: {
+    allowedUsernames: [
+      'gammarers-projen-upgrade-bot[bot]',
+      'yicr',
+    ],
+  },
+  jestOptions: {
+    extraCliOptions: ['--silent'],
+  },
+  tsconfigDev: {
+    compilerOptions: {
+      strict: true,
+    },
+  },
+  lambdaOptions: {
+    // target node.js runtime
+    runtime: awscdk.LambdaRuntime.NODEJS_24_X,
+    bundlingOptions: {
+      // list of node modules to exclude from the bundle
+      externals: ['@aws-sdk/*'],
+      sourcemap: true,
+    },
+  },
 });
+project.addPackageIgnore('/.devcontainer');
 project.synth();
